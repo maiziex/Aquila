189208308c3f5b0ebe748f1f29e681492e41c531
Xin Zhou <xzhou15@stanford.edu>
2019-01-08 23:10:09 -0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,43 @@
+# change permission
+cd bin
+chmod +x *.py
+tar -xzf SPAdes-3.13.0-Linux.tar.gz
+rm SPAdes-3.13.0-Linux.tar.gz
+cd ..
+
+
+# download the reference file (GRCh38)
+wget http://xinzhouneuroscience.org/wp-content/uploads/2018/11/source.tar.gz
+tar -xvf source.tar.gz
+rm source.tar.gz
+
+
+wget http://xinzhouneuroscience.org/wp-content/uploads/2018/11/Uniqness_map.tar.gz
+tar -xvf Uniqness_map.tar.gz
+rm Uniqness_map.tar.gz
+
+
+if ! [ -x "$(command -v samtools)" ];
+then
+    echo 'Error: samtools is not installed...'
+    echo 'using Conda to install...'
+    conda install -c bioconda samtools
+else
+    echo 'using existing samtools...'
+fi
+
+if ! [ -x "$(command -v minimap2)" ];
+then
+    echo 'Error: minimap2 is not installed...'
+    echo 'using Conda to install...'
+    conda install -c bioconda minimap2
+else
+    echo 'using existing minimap2...'
+fi
+
+
+echo 'You have installed XinMagic dependencies and downloaded the source files successfully!'
+ 
+
+
+
